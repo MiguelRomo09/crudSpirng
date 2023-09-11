f7d8c02949f4bcddd4332aa7f05cbf2e691673b6
miguelromo0994 <miguelromo0994@gmail.com>
2023-09-10 22:22:10 -0600

    fix cross

--- src/main/java/jmfr/crud/controlador/EmpleadoControlador.java
+++ src/main/java/jmfr/crud/controlador/EmpleadoControlador.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("crud-app")
-@CrossOrigin(value = "http://localhost:3000")
+@CrossOrigin(value = "https://react-crud-production.up.railway.app/")
 public class EmpleadoControlador {
 
     private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
